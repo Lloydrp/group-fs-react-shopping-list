7263ed1b39f31c9a208cd0ee5b298161ba5026fa
David Vetter <ddvetter23@gmail.com>
2022-09-22 15:00:53 -0500

    actually saved the changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,7 @@ function App() {
             <Header />
             <main>
                 <p>Under Construction...</p>
+                <ShoppingForm getShopping={getShopping} />
             </main>
         </div>
     );
