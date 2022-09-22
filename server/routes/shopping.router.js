6d482dafafe3b7832456914badba3507ce94b286
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-22 13:58:07 -0500

    working PUT purchased

--- server/routes/shopping.router.js
+++ server/routes/shopping.router.js
@@ -23,6 +23,20 @@ router.put("/reset", (req, res) => {
 
 // PUT for /purchase/:id
 
+router.put("/purchased/:shoppingid", (req, res) => {
+  const shoppingid = req.params.shoppingid;
+  const queryText = `UPDATE "list" SET "purchased" = true WHERE "id" = $1;`;
+
+  pool
+    .query(queryText, [shoppingid])
+    .then((results) => {
+      res.send(`Purchased item: ${shoppingid}`).status(200);
+    })
+    .catch((error) => {
+      console.log("error caught in PUT /purchased :>> ", error);
+    });
+});
+
 // DELETE for /clear
 
 // DELETE for /remove/:id
