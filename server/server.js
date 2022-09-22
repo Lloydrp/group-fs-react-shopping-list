1b1483bbf540164baac35a512a020cd792b2cbd3
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-22 11:47:42 -0500

    cleaned server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for axios requests
+app.use(express.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
