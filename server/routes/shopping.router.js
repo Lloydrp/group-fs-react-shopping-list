3f549e17228613bd4ebf9b756bb73074c5800517
Lee Baesler <lee.baesler@gmail.com>
2022-09-22 13:52:20 -0500

    GET request completed

--- server/routes/shopping.router.js
+++ server/routes/shopping.router.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const pool = require("../modules/pool");
 
 // GET Request
+router.get('/', function (req, res) {
+    console.log('in GET route');
+    const query = 'SELECT * FROM "list";';
+    pool.query(query).then((result) => {
+        console.log('GET request', result);
+        res.send(result.row);
+    }).catch((error) => {
+        console.log('ERRROR making GET', error);
+        res.sendStatus(500);
+    })
+}); //END GET ROUTE
 
 // POST Request
 
