254ac9c9c0fc4344008bfdc37ff65f3c80368ea3
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-22 11:43:49 -0500

    Initial commit for branching

--- database.sql
+++ database.sql
@@ -1,2 +1,19 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- DB Name: fs-react-shopping
+
+CREATE TABLE "list" (
+	id serial PRIMARY KEY NOT NULL,
+	item varchar(80) NOT NULL,
+	quantity decimal NOT NULL,
+	unit varchar(20),
+	purchased boolean default false);
+	
+INSERT INTO "list" ("item", "quantity", "unit")
+VALUES ('Apples','5','lbs'),
+('Bread','1','loaf'),
+('Milk','1','gallon'),
+('Bananas','2','bunches');
+
+SELECT * FROM "list";
