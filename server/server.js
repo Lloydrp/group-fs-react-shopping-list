76db9bc13c5cf6c8c45d5b8b52107db235ca8821
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-22 13:30:43 -0500

    added router to server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
+const shoppingRouter = require("./routes/shopping.router");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json()); // needed for axios requests
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
 
+app.use("/shopping", shoppingRouter);
+
 /** ---------- START SERVER ---------- **/
-app.listen(PORT,  () => {
-    console.log('Listening on port: ', PORT);
+app.listen(PORT, () => {
+  console.log("Listening on port: ", PORT);
 });
