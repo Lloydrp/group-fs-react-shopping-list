89d37331d5e26eb8f644e9585633ce05e05c77aa
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-22 13:29:28 -0500

    added router

---
+++ server/routes/shopping.router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../modules/pool");
+
+// GET Request
+
+// POST Request
+
+// PUT for /reset
+
+// PUT for /purchase/:id
+
+// DELETE for /clear
+
+// DELETE for /remove/:id
+
+module.exports = router;
