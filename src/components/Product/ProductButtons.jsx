573305d3e62bd7c31c915d0aec3455665cd33782
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-23 13:12:14 -0500

    working final of updated component

--- src/components/Product/ProductButtons.jsx
+++ src/components/Product/ProductButtons.jsx
@@ -3,17 +3,6 @@ import axios from "axios";
 import { useState } from "react";
 
 function ProductButtons(props) {
-  const iconConfirm = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      viewBox="0 0 16 16"
-    >
-      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-    </svg>
-  );
   const iconEdit = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -73,7 +62,7 @@ function ProductButtons(props) {
         className="item-btn-confirm"
         onClick={() => buyItem(props.product.id)}
       >
-        {iconConfirm}
+        {props.iconConfirm}
       </button>
       <button className="item-btn-edit" onClick={() => editToggle()}>
         {iconEdit}
