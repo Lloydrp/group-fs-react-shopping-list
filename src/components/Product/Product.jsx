00ee2d65bc98782d9cbe2c7cbf0ae1d8a7858517
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-23 12:07:46 -0500

    working edit button w inputs

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,34 +1,97 @@
 import "./Product.css";
 import ProductButtons from "./ProductButtons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Product(props) {
   const [toggleMode, setToggleMode] = useState(false);
+  const [newItem, setNewItem] = useState("");
+  const [newQuantity, setNewQuantity] = useState("");
+  const [newUnit, setNewUnit] = useState("");
 
-  return (
-    <div className={props.product.purchased ? "card purchased-item" : "card"}>
-      <div className="card-item">
-        <p className="cardP">{props.product.item}</p>
-      </div>
-      <div className="card-item">
-        <p className="cardP">
-          {props.product.quantity} {props.product.unit}
-        </p>
-      </div>
-      <div className="card-item">
-        {props.product.purchased ? (
-          <span className="purchased-text">Purchased!</span>
-        ) : (
-          <ProductButtons
-            getShoppingList={props.getShoppingList}
-            product={props.product}
-            setToggleMode={setToggleMode}
-            toggleMode={toggleMode}
+  function updateProduct(id) {
+    axios({
+      method: "PUT",
+      url: `/edit/${id}`,
+      data: {
+        item: newItem,
+        quantity: newQuantity,
+        unit: newUnit,
+      },
+    })
+      .then(() => {
+        getShoppingList();
+      })
+      .catch((error) => {
+        console.log("error caught in edit PUT :>> ", error);
+      });
+  }
+
+  useEffect(() => {
+    setNewItem(props.product.item);
+    setNewQuantity(props.product.quantity);
+    setNewUnit(props.product.unit);
+  }, []);
+
+  if (toggleMode) {
+    return (
+      <div className="card">
+        <div className="card-item">
+          <input
+            className="input-card"
+            value={newItem}
+            type="text"
+            onChange={(event) => setNewItem(event.target.value)}
           />
-        )}
+        </div>
+        <div className="card-item">
+          <input
+            className="input-card input-quantity"
+            value={newQuantity}
+            type="text"
+            onChange={(event) => setNewItem(event.target.value)}
+          />
+          <input
+            className="input-card input-unit"
+            value={newUnit}
+            type="text"
+            onChange={(event) => setNewItem(event.target.value)}
+          />
+        </div>
+        <div className="card-item">
+          <button onClick={() => updateProduct(props.product.id)}>
+            Confirm
+          </button>
+          <button onClick={() => setToggleMode(false)}>Cancel</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={props.product.purchased ? "card purchased-item" : "card"}>
+        <div className="card-item">
+          <p className="cardP">{props.product.item}</p>
+        </div>
+        <div className="card-item">
+          <p className="cardP">
+            {props.product.quantity} {props.product.unit}
+          </p>
+        </div>
+        <div className="card-item">
+          {props.product.purchased ? (
+            <span className="purchased-text">Purchased!</span>
+          ) : (
+            <ProductButtons
+              getShoppingList={props.getShoppingList}
+              product={props.product}
+              setToggleMode={setToggleMode}
+              toggleMode={toggleMode}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Product;
